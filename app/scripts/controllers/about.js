a1f8fd2c53077753506dc9b4144b30573bface3b
Daniel Ryan <danielryan@Daniels-MacBook-Pro.local>
2015-10-26 21:55:55 -0600

    generated yeoman code

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pbrApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pbrApp
+ */
+angular.module('pbrApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pbrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pbrApp
+ */
+angular.module('pbrApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
